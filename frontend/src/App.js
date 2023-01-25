c36eec9bc9dc9c1c979e63473f2e94ac64a04654
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-25 23:29:58 +0300

    add about route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./routes/navigation/navigation.route";
 import Home from "./routes/home/home.route";
+import About from "./routes/about/about.route";
 import NotFound from "./routes/not-found/not-found.route";
 
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
